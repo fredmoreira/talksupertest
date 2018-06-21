36eca20b359dc229e6314e5f34e877bed5670a92
fredmoreira <fredmacc@gmail.com>
2018-06-21 12:29:44 -0300

    Schema included for validation

---
+++ test/schema.js
@@ -0,0 +1,10 @@
+const Joi = require('joi')
+
+const schema_phonebook = Joi.array.items({
+  _id: Joi.string().max(24),
+  name: Joi.string(),
+  mobilephone: Joi.string(),
+  homephone: Joi.string()
+});
+
+module.exports = schema_phonebook
